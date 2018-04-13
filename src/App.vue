b97cf05d8055a2a33850b86a2b36185c6e06e3c0
itingkong <luoqi20131001@163.com>
2018-04-13 22:35:33 +0800

    。。。

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
     <div id="app">
         <img src="./assets/logo.png">
-        <router-view/>
+        <HelloWorld></HelloWorld>
     </div>
 </template>
 
 <script>
+    import HelloWorld from '@/components/HelloWorld'
     export default {
-        name: 'App'
+        name: 'App',
+        components: { HelloWorld }
     }
 </script>
 
